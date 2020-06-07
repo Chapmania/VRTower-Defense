da1e73997c7a271bd6eaa4fc9a726e8190113709
mayan <mshoshan@uci.edu>
2020-06-07 01:21:52 -0700

    built map, added telaport, and goal

---
+++ VRTowerDefense/Assets/_BowAndArrow/Scripts/Goal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class Goal : MonoBehaviour
+{
+    private int health;
+    public Text healthUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 5;
+        healthUI.text = "Health: "+health.ToString();
+    }
+
+    public void Attack()
+     {
+        health--;
+        healthUI.text = "Health: " + health.ToString();
+    }
+
+    private void Update()
+    {
+        if(health<=0)
+            SceneManager.LoadScene(0);
+
+
+    }
+
+
+}
---
+++ VRTowerDefense/Assets/_BowAndArrow/Scripts/Telaport.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telaport : MonoBehaviour
+{
+    public GameObject position1 = null;
+    public GameObject position2 = null;
+    public GameObject position3 = null;
+    public GameObject position4 = null;
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.RawButton.A))
+            transform.position = position1.transform.position;
+
+        if (OVRInput.GetDown(OVRInput.RawButton.B))
+            transform.position = position2.transform.position;
+
+        if (OVRInput.GetDown(OVRInput.RawButton.X))
+            transform.position = position3.transform.position;
+
+        if (OVRInput.GetDown(OVRInput.RawButton.Y))
+            transform.position = position4.transform.position;
+    }
+}
